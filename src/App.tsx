cf4ebe24007b20d55863ea0e25327508c2f7b74d
John Doe <johndoe@example.com>
2023-02-18 23:18:25 +0300

    project fully completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import {Route,Routes} from 'react-router-dom'
 import { AboutPage } from './pages/AboutPage';
 import { ProductPage } from './pages/ProductPage';
-
+import { UseNavigate } from './pages/UseNavigate';
 
 function App() {
 return(
+  <>
+  <UseNavigate/>
   <Routes>
     <Route path='/' element={<ProductPage/>}/>
 <Route path='/about' element={<AboutPage/>}/>
   </Routes>
+  </>
 )
 }
 
---
+++ src/pages/UseNavigate.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+export const UseNavigate = () => {
+  return (
+    <nav className='h-[50px] flex justify-between px-5 bg-gray-500 items-center text-white'>
+        <span>React 2023</span>
+
+        <span>
+            <Link to='/' className='mr-2' >Products</Link>
+            <Link to='/about' >About</Link>
+        </span>
+    </nav>
+  )
+}
